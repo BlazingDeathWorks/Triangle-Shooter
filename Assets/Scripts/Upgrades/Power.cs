42eb368feb5dd337d8bd009d6e1724beedae3958
Jason Chan <shunsei.chan@gmail.com>
2022-04-08 11:32:22 -0700

    Testing Power.cs Awake

--- Assets/Scripts/Upgrades/Power.cs
+++ Assets/Scripts/Upgrades/Power.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Power : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Hi");
     }
 }
