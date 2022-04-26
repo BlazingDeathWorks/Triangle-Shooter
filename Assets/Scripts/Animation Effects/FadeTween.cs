284da695cd18152d585844beba403c809b85b6cb
Jason Chan <shunsei.chan@gmail.com>
2022-04-26 14:30:55 -0700

    Ghost Runner (Not Finished)

---
+++ Assets/Scripts/Animation Effects/FadeTween.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeTween : MonoBehaviour
+{
+    [SerializeField] private float _fadeTime = 0.25f;
+    [SerializeField] private float _alphaTo = 1;
+
+    private void Awake()
+    {
+
+    }
+}
--- Assets/Scripts/Player/PlayerGhostRunner.cs
+++ Assets/Scripts/Player/PlayerGhostRunner.cs
@@ -2,19 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-internal class PlayerGhostRunner : MonoBehaviour
+internal class PlayerGhostRunner : MonoBehaviour, IObjectPooler<NormalPoolableObject>
 {
+    public NormalPoolableObject Prefab => _prefab;
+    public Queue<NormalPoolableObject> Pool { get; } = new Queue<NormalPoolableObject>();
+
+    [SerializeField] private NormalPoolableObject _prefab;
     [SerializeField] private float _timeBetweenGhost = 1;
+    private float _timeSinceLastGhost = 0;
     private PlayerMovementController _playerMovementController;
+    private Transform _transform;
 
     private void Awake()
     {
         _playerMovementController = GetComponent<PlayerMovementController>();
+        _transform = transform;
+        _timeSinceLastGhost = _timeBetweenGhost;
     }
 
     private void Update()
     {
         if (!_playerMovementController.IsMoving) return;
+        if (_timeSinceLastGhost >= _timeBetweenGhost)
+        {
+            _timeSinceLastGhost = 0;
+            ObjectPool.Pool(this);
+            return;
+        }
+        _timeSinceLastGhost += Time.deltaTime;
+    }
 
+    public void OnPooled(NormalPoolableObject instance)
+    {
+        instance.gameObject.SetActive(true);
+        instance.transform.localEulerAngles = _transform.localEulerAngles;
+        instance.transform.position = _transform.position;
     }
 }
