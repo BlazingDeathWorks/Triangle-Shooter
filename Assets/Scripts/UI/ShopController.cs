4f4603aadafca412a94b68c914172b769c5cf5d1
Jason Chan <shunsei.chan@gmail.com>
2022-04-30 13:19:26 -0700

    Added Timescale to ShopController

--- Assets/Scripts/UI/ShopController.cs
+++ Assets/Scripts/UI/ShopController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ShopController : MonoBehaviour
 {
     [SerializeField] private Tab _shopTab;
+    [SerializeField][Range(0.1f, 1)] private float _timeScaleInShop = 0.5f;
     [SerializeField] private InputButton _shopInput;
 
     void Update()
@@ -14,9 +15,11 @@ public class ShopController : MonoBehaviour
             if (_shopTab.gameObject.activeSelf)
             {
                 _shopTab.gameObject.SetActive(false);
+                Time.timeScale = 1;
                 return;
             }
             _shopTab.Enable();
+            Time.timeScale = _timeScaleInShop;
         }
     }
 }
