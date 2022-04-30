5cfc525df7cf22dae60b747538a6937fac075e8f
Jason Chan <shunsei.chan@gmail.com>
2022-04-29 22:53:46 -0700

    Action Channel (Bool)

---
+++ Assets/Scripts/Scriptable Objects/SO Channels/Actions/ActionChannel_Bool.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewActionChannel_Bool", menuName = "SO Channels/Action Channel (Bool)")]
+public class ActionChannel_Bool : ScriptableObject, IActionChannelGenerics1<Boolean>
+{
+    private Action<bool> func;
+
+    public void AddAction(Action<bool> func)
+    {
+        this.func += func;
+    }
+
+    public void CallAction(bool var1)
+    {
+        func?.Invoke(var1);
+    }
+
+    public void RemoveAction(Action<bool> func)
+    {
+        this.func -= func;
+    }
+}
