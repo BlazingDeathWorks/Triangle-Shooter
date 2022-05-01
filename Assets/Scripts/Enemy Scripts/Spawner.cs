e8b150955dbacd9eff99f33695ec56846f4a063b
Jason Chan <shunsei.chan@gmail.com>
2022-04-30 17:22:45 -0700

    Upgraded Spawner Timer

--- Assets/Scripts/Enemy Scripts/Spawner.cs
+++ Assets/Scripts/Enemy Scripts/Spawner.cs
@@ -7,28 +7,46 @@ internal class Spawner : MonoBehaviour, IObjectPooler<EnemySpawner>
     public EnemySpawner Prefab => _particle;
     public Queue<EnemySpawner> Pool { get; } = new Queue<EnemySpawner>();
 
-    [SerializeField] EnemySpawner _particle = null;  
+    [SerializeField] EnemySpawner _particle = null;
 
     //Spawn Difficulty
     private const float SPAWN_RATE_MIN = 0.4f, SPAWN_RATE_MAX = 0.8f;
     private const int FREQUENCY = 20;
+    private float _timeSinceLastSpawn = 0;
     private float _spawnRate = 0.8f;
     private float _decrementBy = 0.1f;
     private int _spawnRateDecrementRate = 20;
 
-    IEnumerator Start()
+    // IEnumerator Start()
+    // {
+    //     while (true)
+    //     {
+    //         if (Time.time >= _spawnRateDecrementRate)
+    //         {
+    //             _spawnRateDecrementRate += FREQUENCY;
+    //             _spawnRate -= _decrementBy;
+    //             _spawnRate = MathUtil.WrapFloat(_spawnRate, SPAWN_RATE_MIN, SPAWN_RATE_MAX);
+    //         }
+    //         yield return new WaitForSeconds(_spawnRate);
+    //         ObjectPool.Pool(this);
+    //     }
+    // }
+
+    private void Update()
     {
-        while (true)
+        if (Time.time >= _spawnRateDecrementRate)
         {
-            if (Time.time * 10 >= _spawnRateDecrementRate)
-            {
-                _spawnRateDecrementRate += FREQUENCY;
-                _spawnRate -= _decrementBy;
-                _spawnRate = MathUtil.WrapFloat(_spawnRate, SPAWN_RATE_MIN, SPAWN_RATE_MAX);
-            }
-            yield return new WaitForSeconds(_spawnRate);
-            ObjectPool.Pool(this);
+            _spawnRateDecrementRate += FREQUENCY;
+            _spawnRate -= _decrementBy;
+            _spawnRate = MathUtil.WrapFloat(_spawnRate, SPAWN_RATE_MIN, SPAWN_RATE_MAX);
         }
+        if (_timeSinceLastSpawn >= _spawnRate)
+        {
+            _timeSinceLastSpawn = 0;
+            ObjectPool.Pool(this);
+            return;
+        }
+        _timeSinceLastSpawn += Time.deltaTime;
     }
 
     public void OnPooled(EnemySpawner instance)
