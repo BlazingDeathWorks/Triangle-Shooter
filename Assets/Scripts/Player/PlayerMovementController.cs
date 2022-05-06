fd452bbce1cf75893347a04202e78ebce6326fcb
Jason Chan <shunsei.chan@gmail.com>
2022-05-05 20:07:41 -0700

    PlayerMovement System Updated to work with 60 FPS

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -7,11 +7,13 @@ internal class PlayerMovementController : MonoBehaviour
     [SerializeField] private FuncChannel_Bool _playerGhostRunnerBoolEventHandler;
     [SerializeField] private float _speed = 1;
     private float _x, _y;
+    private Transform _transform;
     private Rigidbody2D _rb = null;
 
 
     private void Awake()
     {
+        _transform = transform;
         _rb = GetComponent<Rigidbody2D>();
         _playerGhostRunnerBoolEventHandler.AddAction(() => Mathf.Abs(_x) > 0 || Mathf.Abs(_y) > 0);
     }
@@ -23,12 +25,12 @@ internal class PlayerMovementController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _rb.velocity = (new Vector2(_x, _y)).normalized * _speed;
+        _rb.MovePosition((Vector2)_transform.position + new Vector2(_x, _y).normalized * _speed * Time.deltaTime);
     }
 
     private void SetXY()
     {
-        _x = Input.GetAxis("Horizontal");
-        _y = Input.GetAxis("Vertical");
+        _x = Input.GetAxisRaw("Horizontal");
+        _y = Input.GetAxisRaw("Vertical");
     }
 }
---
+++ Assets/Scripts/UI/FrameRateManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameRateManager : MonoBehaviour
+{
+    public static FrameRateManager Instance { get; private set; }
+    [SerializeField] private int _startingTargetFrameRate = 60;
+
+    private void Awake()
+    {
+        #region Singleton
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        #endregion    
+
+        Application.targetFrameRate = _startingTargetFrameRate;
+    }
+}
