296ae4ab6f4e3cc877af25f5cb89d0bc4a34d661
Jason Chan <shunsei.chan@gmail.com>
2022-05-05 20:56:40 -0700

    Updated LensTween to work with new FPS cap

--- Assets/Scripts/Animation Effects/LensTween.cs
+++ Assets/Scripts/Animation Effects/LensTween.cs
@@ -8,6 +8,7 @@ public class LensTween : MonoBehaviour
 {
     [SerializeField] private float _from, _to;
     [SerializeField] private float _distortTime;
+    private bool _isPlaying = false;
     private Volume _volume;
     private LensDistortion _lensDistortion;
 
@@ -20,18 +21,27 @@ public class LensTween : MonoBehaviour
 
     public void DistortLens()
     {
-        if (_lensDistortion == null) return;
+        if (_lensDistortion == null || _isPlaying) return;
 
+        _isPlaying = true;
         _lensDistortion.intensity.value = _from;
         LeanTween.value(gameObject, _from, _to, _distortTime)
         .setOnUpdate((value) =>
         {
             _lensDistortion.intensity.value = Mathf.Lerp(_lensDistortion.intensity.value, _to, value);
-        });
-        LeanTween.value(gameObject, _from, _to, _distortTime)
-        .setOnUpdate((value) =>
+        })
+        .setOnComplete(() =>
         {
-            _lensDistortion.intensity.value = Mathf.Lerp(_lensDistortion.intensity.value, _from, value);
+            LeanTween.value(gameObject, _from, _to, _distortTime)
+            .setOnUpdate((value) =>
+            {
+                _lensDistortion.intensity.value = Mathf.Lerp(_lensDistortion.intensity.value, _from, value);
+            })
+            .setOnComplete(() =>
+            {
+                _isPlaying = false;
+            });
         });
+
     }
 }
