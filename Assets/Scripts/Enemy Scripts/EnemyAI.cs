93878f43a1ad8e72914bc96a9166d42886c6d4da
cubewar <maxman.chan@gmail.com>
2022-05-20 17:59:37 -0700

    look

--- Assets/Scripts/Enemy Scripts/EnemyAI.cs
+++ Assets/Scripts/Enemy Scripts/EnemyAI.cs
@@ -19,6 +19,10 @@ internal class EnemyAI : MonoBehaviour
     {
         if (_player == null) return;
         _transform.position = Vector2.MoveTowards(_transform.position, _player.position, _speed * Time.deltaTime);
+        var dir = SceneReferenceManager.GetReference(PLAYER).transform.position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
     }
 
 }
