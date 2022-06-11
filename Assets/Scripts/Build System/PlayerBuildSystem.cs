1606c522cc05f6fedf4542c20ff8968439c960a2
Jason Chan <shunsei.chan@gmail.com>
2022-06-11 10:27:48 -0700

    Removed BuildingBlockBase

--- Assets/Scripts/Build System/PlayerBuildSystem.cs
+++ Assets/Scripts/Build System/PlayerBuildSystem.cs
@@ -2,19 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-internal class PlayerBuildSystem : MonoBehaviour, IObjectPooler<BuildingBlockBase>, IObjectPooler<BuildingBlock>
+internal class PlayerBuildSystem : MonoBehaviour, IObjectPooler<BuildingBlock>
 {
-    public BuildingBlockBase Prefab => _buildingBlockBase;
-    public Queue<BuildingBlockBase> Pool { get; } = new Queue<BuildingBlockBase>();
-
     BuildingBlock IObjectPooler<BuildingBlock>.Prefab => _buildingBlock;
     Queue<BuildingBlock> IObjectPooler<BuildingBlock>.Pool { get; } = new Queue<BuildingBlock>();
 
     [SerializeField] private InputButton _buildInput;
     [SerializeField] private Transform _buildBlockBaseContainer;
-    [SerializeField] private BuildingBlockBase _buildingBlockBase;
     [SerializeField] private BuildingBlock _buildingBlock;
-    private Transform _previousBlockPos;
     private Transform _transform;
 
     private void Awake()
@@ -26,31 +21,14 @@ internal class PlayerBuildSystem : MonoBehaviour, IObjectPooler<BuildingBlockBas
     {
         if (_buildInput.Clicked)
         {
-            ObjectPool.Pool<BuildingBlockBase>(this);
+            ObjectPool.Pool(this);
         }
-    }
-
-    public void OnPooled(BuildingBlockBase instance)
-    {
-        instance.gameObject.SetActive(true);
-        instance.transform.parent = _buildBlockBaseContainer;
-        instance.transform.position = _transform.position;
-        if (_previousBlockPos == null)
-        {
-            _previousBlockPos = instance.transform;
-            return;
-        }
-        ObjectPool.Pool<BuildingBlock>(this);
-        _previousBlockPos = instance.transform;
     }
 
     public void OnPooled(BuildingBlock instance)
     {
         instance.gameObject.SetActive(true);
         instance.transform.parent = _buildBlockBaseContainer;
-        float distance = Vector2.Distance(_previousBlockPos.position, _transform.position);
-        instance.transform.localScale = new Vector2(distance, instance.transform.localScale.y);
-        instance.transform.position = (_previousBlockPos.position + _transform.position) / 2;
-        instance.transform.localEulerAngles = new Vector3(0, 0, Mathf.Rad2Deg * Mathf.Atan2(_transform.position.y - _previousBlockPos.position.y, _transform.position.x - _previousBlockPos.position.x));
+        //instance.transform.localScale = new Vector2(distance, instance.transform.localScale.y);
     }
 }
