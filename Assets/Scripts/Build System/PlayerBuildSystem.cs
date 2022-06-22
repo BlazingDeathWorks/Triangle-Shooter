990e2c97c6f5e1ee176e17dfcbeb1e1b702612a8
Jason Chan <shunsei.chan@gmail.com>
2022-06-21 18:19:41 -0700

    Revert "Build System Game Design Polish"
    This reverts commit c3ba2ccffb76b98fd2c96b8479cbf842d7138b3f.

--- Assets/Scripts/Build System/PlayerBuildSystem.cs
+++ Assets/Scripts/Build System/PlayerBuildSystem.cs
@@ -24,11 +24,12 @@ public class PlayerBuildSystem : MonoBehaviour, IUpgradable
 
     private void Update()
     {
+        if (_buildActivatedEventHandler == null) return;
         if (_buildInput.Clicked)
         {
             _gridContainer.position = _transform.position;
             _gridContainer.gameObject.SetActive(!_gridContainer.gameObject.activeSelf);
-            _buildActivatedEventHandler?.CallAction(_gridContainer.gameObject.activeSelf);
+            _buildActivatedEventHandler.CallAction(_gridContainer.gameObject.activeSelf);
             Time.timeScale = _gridContainer.gameObject.activeSelf ? _slowTime : 1;
         }
     }
@@ -39,10 +40,10 @@ public class PlayerBuildSystem : MonoBehaviour, IUpgradable
         {
             _gridContainer.gameObject.SetActive(false);
             _buildActivatedEventHandler.CallAction(_gridContainer.gameObject.activeSelf);
-            gameObject.SetActive(false);
+            enabled = false;
             return;
         }
-        gameObject.SetActive(true);
+        enabled = true;
     }
 
     //Creates Build Grid Boxes
--- Assets/Scripts/Player/PlayerShootController.cs
+++ Assets/Scripts/Player/PlayerShootController.cs
@@ -2,29 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerRotation))]
 internal class PlayerShootController : MonoBehaviour, IObjectPooler<Bullet>
 {
     public Bullet Prefab => _bullet;
     public Queue<Bullet> Pool { get; } = new Queue<Bullet>();
 
-    [SerializeField] private ActionChannel_Bool _buildActivatedEventHandler;
-    [SerializeField] private Transform _bulletSpawnPoint = null;
-    [SerializeField] private Bullet _bullet = null;
-    [SerializeField] private ShakeTween _shakeTween = null;
-    [SerializeField] private float _timeBetweenBullets = 1;
-    private float _time;
-    private PlayerRotation _playerRotation;
+    [SerializeField] Transform _bulletSpawnPoint = null;
+    [SerializeField] Bullet _bullet = null;
+    [SerializeField] Camera _camera = null;
+    [SerializeField] ShakeTween _shakeTween = null;
+    [SerializeField] float _timeBetweenBullets = 1;
+
+    Transform _transform = null;
+    Vector2 _mousePos, _direction;
+    float _angle = 0;
+    float _time;
+
+    public void OnPooled(Bullet instance)
+    {
+        instance.gameObject.SetActive(true);
+        instance.transform.position = _bulletSpawnPoint.position;
+        instance.Direction = _direction;
+    }
 
     private void Awake()
     {
-        _playerRotation = GetComponent<PlayerRotation>();
-        _buildActivatedEventHandler?.AddAction(OnBuildActivated);
+        _transform = transform;
         _time = _timeBetweenBullets;
     }
 
     private void Update()
     {
+        //Rotation
+        _mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
+        _direction = _mousePos - (Vector2)_transform.position;
+        _angle = Mathf.Rad2Deg * Mathf.Atan2(_direction.y, _direction.x);
+
+        _transform.localEulerAngles = new Vector3(0, 0, _angle - 90);
+
         //Shooting
         _time += Time.deltaTime;
         if (Input.GetKey(KeyCode.Mouse0))
@@ -36,23 +51,5 @@ internal class PlayerShootController : MonoBehaviour, IObjectPooler<Bullet>
                 _shakeTween?.Shake();
             }
         }
-    }
-
-    private void OnBuildActivated(bool isActive)
-    {
-        if (isActive)
-        {
-            enabled = false;
-            _time = _timeBetweenBullets;
-            return;
-        }
-        enabled = true;
-    }
-
-    public void OnPooled(Bullet instance)
-    {
-        instance.gameObject.SetActive(true);
-        instance.transform.position = _bulletSpawnPoint.position;
-        instance.Direction = _playerRotation.Direction;
     }
 }
