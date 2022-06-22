c398e571c25bd3ad86e44220f5bbd9a754df02dd
Jason Chan <shunsei.chan@gmail.com>
2022-06-21 18:21:54 -0700

    Revert "bruh"
    This reverts commit 31d67e0d5b024d730450f67f3373c4112a7ee4eb, reversing
    changes made to c3ba2ccffb76b98fd2c96b8479cbf842d7138b3f.

--- Assets/Scripts/UI/ShopController.cs
+++ Assets/Scripts/UI/ShopController.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class ShopController : MonoBehaviour
 {
+    [SerializeField] private ActionChannel_Bool _shopActivatedEventHandler;
     [SerializeField] private Tab _shopTab;
     [SerializeField] private InputButton _shopInput;
 
     void Update()
     {
+        //You can just delete this if statement when you need it
         if (_shopInput.Clicked)
         {
+            _shopActivatedEventHandler?.CallAction(!_shopTab.gameObject.activeSelf);
             if (_shopTab.gameObject.activeSelf)
             {
                 _shopTab.gameObject.SetActive(false);
+                Time.timeScale = 1;
                 return;
             }
+            Time.timeScale = 0;
             _shopTab.Enable();
         }
     }
