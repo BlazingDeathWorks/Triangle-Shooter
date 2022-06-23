96aced42b10a0fba474b352898d49d0f521bf007
cubewar <maxman.chan@gmail.com>
2022-06-22 20:25:27 -0700

    level system basic

--- Assets/Scripts/Player/LevelSystem.cs
+++ Assets/Scripts/Player/LevelSystem.cs
@@ -7,16 +7,32 @@ public class LevelSystem : MonoBehaviour
 {
     [SerializeField] Slider expText = null;
     [SerializeField] float xp = 0;
-    [SerializeField] float limit = 100;
+    [SerializeField] float limit = 10;
     [SerializeField] ActionChannel giveXp;
+    [SerializeField] private ActionChannel_Bool _shopActivatedEventHandler;
+    [SerializeField] private GameObject _shopTab;
+    [SerializeField] private InputButton _shopInput;
     private void Awake()
     {
         expText.value = xp/limit;
         giveXp.AddAction(UpdateXp);
     }
     void UpdateXp() {
-        expText.value = xp/limit;
-        if (xp < limit) xp++;
+        expText.value = xp / limit;
+        if (xp < limit) { xp++; }
+        else {
+            limit += 20;
+            xp = 0;
+            _shopActivatedEventHandler?.CallAction(!_shopTab.gameObject.activeSelf);
+            if (_shopTab.gameObject.activeSelf)
+            {
+                _shopTab.gameObject.SetActive(false);
+                Time.timeScale = 1;
+                return;
+            }
+            Time.timeScale = 0;
+            _shopTab.SetActive(true); 
+        }
     }
 
 }
