112dbdf1a1d9e47f2add6acaa6c3539d8a93f923
Jason Chan <shunsei.chan@gmail.com>
2022-06-25 11:06:52 -0700

    Deleted Manual Shop Control
    Just change the xp or limit of Level System script for testing

--- Assets/Scripts/UI/ShopController.cs
+++ Assets/Scripts/UI/ShopController.cs
@@ -4,30 +4,5 @@ using UnityEngine;
 
 public class ShopController : MonoBehaviour
 {
-    [SerializeField] private ActionChannel_Bool _shopActivatedEventHandler;
-    [SerializeField] private Tab _shopTab;
-    [SerializeField] private InputButton _shopInput;
-    [SerializeField] private bool _developerControls = true;
-
-    //Leave this as a shop controller for developers
-    private void Awake()
-    {
-        if (!_developerControls) enabled = false;
-    }
-
-    void Update()
-    {
-        if (_shopInput.Clicked)
-        {
-            _shopActivatedEventHandler?.CallAction(!_shopTab.gameObject.activeSelf);
-            if (_shopTab.gameObject.activeSelf)
-            {
-                _shopTab.gameObject.SetActive(false);
-                Time.timeScale = 1;
-                return;
-            }
-            Time.timeScale = 0;
-            _shopTab.Enable();
-        }
-    }
+    
 }
