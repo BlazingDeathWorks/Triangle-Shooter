a185eec44ece7335a5bd03ae21cfff78aace6038
Jason Chan <shunsei.chan@gmail.com>
2022-06-26 21:19:17 -0700

    Player Max Health Variants Completed

--- Assets/Scripts/Player/PlayerMaxHealthUpgradable.cs
+++ Assets/Scripts/Player/PlayerMaxHealthUpgradable.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //Upgrade to increase max health of player
-public class PlayerMaxHealthUpgradable : MonoBehaviour, IUpgradable
+public class PlayerMaxHealthUpgradable : MonoBehaviour, IUpgradable, IUpgradableVariants
 {
     public float MaxHealth { get; private set; } = 3;
     [SerializeField] private PlayerHealthSystem _playerHealthSystem;
+    private float _percentFactor = 0;
 
     public void OnUpgrade()
     {
-        if (MaxHealth % 5 == 0)
-        {
-            MaxHealth += 3;
-        }
-        else
-        {
-            MaxHealth += 2;
-        }
+        MaxHealth += Mathf.Round(_percentFactor * MaxHealth);
         _playerHealthSystem?.UpdateHealth();
+    }
+
+    public void Init(PowerData data)
+    {
+        _percentFactor = Random.Range(1, 11) / 10.0f;
+        data.Description = $"{_percentFactor}";
     }
 }
