f15584dfba695e992bc83e3d54cc016d176031ab
Jason Chan <shunsei.chan@gmail.com>
2022-06-29 21:14:12 -0700

    Updated Scripts

--- Assets/Scripts/Equipment/EquipmentModel.cs
+++ Assets/Scripts/Equipment/EquipmentModel.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 internal class EquipmentModel : MonoBehaviour
 {
+    //Change type GameObject to Equipment Base class later...
+    [SerializeField] private GameObject _equipmentPrefab;
+    [SerializeField] private Sprite _modelDisplay;
     private EquipmentModelManager _modelManager;
 
     private void Awake()
--- Assets/Scripts/Equipment/EquipmentModelManager.cs
+++ Assets/Scripts/Equipment/EquipmentModelManager.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 internal class EquipmentModelManager : MonoBehaviour
 {
+    [SerializeField] private Transform _equipmentPosition;
+    private int _index = 0;
+    private List<EquipmentModel> _equipmentModels = new List<EquipmentModel>();
+
     private void Awake()
     {
+        //Disables everything except for the first children
         int i = 0;
         foreach (Transform child in transform)
         {
-            if (i == 0) continue;
+            if (i == _index) continue;
             child.gameObject.SetActive(false);
+            _equipmentModels.Add(child.GetComponent<EquipmentModel>());
             i++;
         }
     }
