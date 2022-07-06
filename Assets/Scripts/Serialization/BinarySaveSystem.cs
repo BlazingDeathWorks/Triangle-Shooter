7307814d93de76985322e10040b46ef6c0df341d
Jason Chan <shunsei.chan@gmail.com>
2022-07-05 21:53:47 -0700

    Added Serialization Scripts

---
+++ Assets/Scripts/Serialization/BinarySaveSystem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public static class BinarySaveSystem
+{
+    public static void SaveSystem<T>(T data, string path) where T : class
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+        {
+            bf.Serialize(fs, data);
+        }
+    }
+
+    public static T LoadSystem<T>(string path) where T : class
+    {
+        if (!File.Exists(path)) return default;
+        BinaryFormatter bf = new BinaryFormatter();
+        using (FileStream fs = new FileStream(path, FileMode.Open))
+        {
+            return bf.Deserialize(fs) as T;
+        }
+    }
+}
