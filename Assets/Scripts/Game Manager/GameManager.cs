c8b35a0f6fe192e174c08dc7248dd1019a4dcebe
Jason Chan <shunsei.chan@gmail.com>
2022-07-06 18:02:57 -0700

    Base setup of Best Score Update System
    TODO: Actually add the score system (new manager or something)

--- Assets/Scripts/Game Manager/GameManager.cs
+++ Assets/Scripts/Game Manager/GameManager.cs
@@ -3,23 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-internal class GameManager : MonoBehaviour
+internal sealed class GameManager : MonoBehaviour
 {
     [SerializeField] private ActionChannel _gameStartedEventHandler;
     [SerializeField] private ActionChannel _playerDiedEventHandler;
     [SerializeField] private GameObject _gameOverTab;
+    private Text _bestScoreText;
+    private const string FILE_NAME = "/Best Score.bin";
+    private string _path;
 
     private void Awake()
     {
+        _path = Application.persistentDataPath + FILE_NAME;
         _gameOverTab.SetActive(false);
         _gameStartedEventHandler?.CallAction();
         _playerDiedEventHandler?.AddAction(() => StartCoroutine(GameOver()));
-        Debug.Log(_gameOverTab.GetComponentInChildren<Text>().text);
+        _bestScoreText = _gameOverTab.GetComponentInChildren<Text>();
     }
 
     private IEnumerator GameOver()
     {
         yield return new WaitForSecondsRealtime(2f);
+        //LoadBestScore();
         _gameOverTab.SetActive(true);
     }
+
+    private void LoadBestScore()
+    {
+        ScoreBoard highScore = BinarySaveSystem.LoadSystem<ScoreBoard>(_path);
+        int score = 0;
+
+        //Update Best Score with Current Score to system: highScore == null || highScore.Score < currentScore
+        if (highScore == null)
+        {
+            
+        }
+
+        //No need for updating score to system
+        else
+        {
+            score = highScore.Score;
+        }
+
+        _bestScoreText.text = $"BEST SCORE: {score}";
+    }
+
+    private class ScoreBoard
+    {
+        public int Score;
+
+        public ScoreBoard(int score)
+        {
+            Score = score;
+        }
+    } 
 }
