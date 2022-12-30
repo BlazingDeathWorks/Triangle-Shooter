d87036c6ea89b0700aaa3d2a9b20bd1363411370
Jason Chan <shunsei.chan@gmail.com>
2022-12-30 15:56:24 -0800

    Login Progress

--- Assets/Scripts/Authentication/LoginManager.cs
+++ Assets/Scripts/Authentication/LoginManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LootLocker.Requests;
+using UnityEngine.UI;
 
-public class LoginManager : MonoBehaviour
+internal class LoginManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private InputField _username;
+    [SerializeField] private InputField _password;
+    [SerializeField] private Text _errorText;
+
+    public void SignUp()
     {
-        
+        LootLockerSDKManager.WhiteLabelSignUp(_username?.text, _password?.text, (response) =>
+        {
+            if (!response.success)
+            {
+                //Fix this so we cut out everything except for the actual message
+                _errorText.text = response.Error;
+                return;
+            }
+
+            Debug.Log("user created successfully");
+            Login();
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Login()
     {
-        
+        LootLockerSDKManager.WhiteLabelLogin(_username.text, _password.text, response =>
+        {
+            if (!response.success)
+            {
+                //Fix this so we cut out everything except for the actual message
+                _errorText.text = response.Error;
+                return;
+            }
+
+            string token = response.SessionToken;
+
+            // Start game session here
+        });
     }
 }
