395d36ba1194587ed67dcdd088dfba37353008fc
Jason Chan <shunsei.chan@gmail.com>
2023-01-01 18:34:22 -0800

    Enemy AI

--- Assets/Scripts/Enemy Scripts/EnemyAI.cs
+++ Assets/Scripts/Enemy Scripts/EnemyAI.cs
@@ -4,26 +4,54 @@ using UnityEngine;
 
 internal class EnemyAI : MonoBehaviour
 {
-    [SerializeField] private float _speed =5f;
+    [SerializeField] private float _magnitudeLimit = 8;
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _rushRate = 1;
     private Rigidbody2D _rb;
     private Transform _player;
+    private Transform _transform;
     const string PLAYER = "Player";
+    private float _timeSinceLastRush = 0;
+    private Vector3 dir;
+
     void Awake() 
     {
+        _transform = transform;
         _rb = gameObject.GetComponent<Rigidbody2D>();
-        _player = GameObject.FindGameObjectWithTag(PLAYER)?.GetComponent<Transform>();
+        _player = SceneReferenceManager.GetReference(PLAYER).transform;
     }
 
     void Update() 
     {
         if (_player == null) return;
 
-        var dir = _player.position - transform.position;
-       
-        //_rb.velocity = dir*_speed*Time.deltaTime;
-        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        _rb.AddForce(dir*_speed*Time.deltaTime);
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        _timeSinceLastRush += Time.deltaTime;
+        dir = _player.position - _transform.position;
 
+        if (_timeSinceLastRush >= _rushRate)
+        {
+            _timeSinceLastRush = 0;
+            RushPlayer();
+            return;
+        }
+
+        MoveTowardsPlayer();
+
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        _rb.velocity = dir.normalized * _speed;
+    }
+
+    private void RushPlayer()
+    {
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (_rb.velocity.magnitude >= _magnitudeLimit)
+        {
+            _rb.AddForce(-dir * _speed);
+        }
+        _rb.AddForce(dir * _speed, ForceMode2D.Force);
+        _transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
